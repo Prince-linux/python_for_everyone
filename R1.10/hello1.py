48eab64b11b71e4f0b6bd271534f0da7b0bae6b8
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-02-20 11:05:53 +0000

    Prince: Finsihed Review Question 10

---
+++ R1.10/hello1.py
@@ -0,0 +1,10 @@
+''' A python file that prints hello world to the screen with a compile-time error
+
+Question: R1.10: Write three versions of the hello.py program that have different compile-time errors.
+
+Author: Prince Oppong Boamah<regioths@gmail.com>
+'''
+
+print("hello, World!')
+
+''' the code above has a mismatch of quotation marks '''
---
+++ R1.10/hello3.py
@@ -0,0 +1,10 @@
+''' A python program that prints hello world with a compile-time error
+
+Question: R1.10: Write three versions of the hello.py program that have different compile-time errors.
+
+Author: Prince Oppong Boamah<regioths@gmail.com>
+'''
+print "Hello World"
+
+''' The code above has a missing paranthesis(both)'''
+
