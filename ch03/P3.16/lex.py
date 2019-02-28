5b79b640af3fe6dc57a7e06b2e7c725451393890
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-02-28 21:42:57 +0000

    Prince: Modified this exercise

--- ch03/P3.16/lex.py
+++ ch03/P3.16/lex.py
@@ -6,6 +6,9 @@
 #Baker 
 #Charlie
 
+#Author: Prince Oppong Boamah<regioths@gmail.com>
+#Date: 28th February, 2019
+
 string1 = input("Please enter three strings: ")
 string2 = input("Please enter another string: ")
 string3 = input("Please enter your last string: ")
