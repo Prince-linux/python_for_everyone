e3245824172a17bde458f68c15e3bf03e02ad877
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-02-28 20:36:36 +0000

    Prince: Solved this exercise

---
+++ ch03/P3.16/lex.py
@@ -0,0 +1,21 @@
+#Question: P3.16: Write a program that reads in three strings and sorts them lexicographically.
+#Enter a string: Charlie 
+#Enter a string: Able 
+#Enter a string: Baker 
+#Able
+#Baker 
+#Charlie
+
+string1 = input("Please enter three strings: ")
+string2 = input("Please enter another string: ")
+string3 = input("Please enter your last string: ")
+
+words= [string1, string2, string3]
+srted = sorted(words)
+words.sort()
+
+print(words[0])
+print(words[1])
+print(words[2])
+
+
