1abb98433f4b9a956f46e1bfcf73e629b5aedb0e
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-03-05 18:00:47 +0000

    Prince: Completed this exercise

---
+++ ch04/P4.8/letter.py
@@ -0,0 +1,16 @@
+#Question: P4.8: Write a program that reads a word and prints each character of the word on a sepa- rate line. 
+#For example, if the user provides the input "Harry", the program prints
+#H
+#a 
+#r 
+#r 
+#y
+
+#Author: Prince Oppong Boamah<regioths@gmail.com>
+#Date: 5th March, 2019
+
+letter = input("Please enter any string: ")
+for i in letter:
+	print(i)
+
+
