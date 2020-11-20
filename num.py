542c79333cfd1a319fcd9b2e8b8ec5e5fa356635
Prince Boamah <princeboamah@Princes-MacBook-Air.local>
2020-11-20 12:33:15 +0000

    Prince: ref #1 Added num.py

---
+++ num.py
@@ -0,0 +1,17 @@
+number = int(input("Please enter any number: "))
+
+if number % 4 ==0:
+    print("Multiple of 4")
+elif number % 2 ==0:
+    print("%s is an Even Number" %(str(number))) 
+else:
+     print("%s is an Odd Number" %(str(number)))
+
+
+num = int(input("Please enter first number: "))
+check = int(input("Please enter second number: "))
+if num % check == 0:
+    print("Both numbers Divide evenly")
+else:
+    print("Can't Divide both numbers evenly")
+
