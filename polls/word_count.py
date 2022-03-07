e4848946a7b2a3e298e97d3b2673da9d5c4d0f12
Prince Boamah <regioths@gmail.com>
2022-03-07 22:30:02 +0000

    A word count program

---
+++ polls/word_count.py
@@ -0,0 +1,8 @@
+# A word count problem solving in python
+
+# Author: Prince Oppong Boamah<regioths@gmail.com>
+# Date: 7th March, 2022
+
+line = input()
+total_words = line.count(' ') + 1
+print(total_words)
