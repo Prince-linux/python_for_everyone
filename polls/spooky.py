444a0620b95b5dfec523dfc5ba777b5dc23ac159
Prince Boamah <regioths@gmail.com>
2022-03-08 10:40:01 +0000

    A Spooky season program

---
+++ polls/spooky.py
@@ -0,0 +1,10 @@
+# A spooky program that prints spooky depending on the amount of 'o's being input
+# Author: Prince Oppong Boamah<regioths@gmail.com>
+# Date: 8th March, 2022
+
+user = input()
+first = 'sp'
+second = 'o' * user
+third = 'ky'
+complete = first + second + third
+print(complete)
