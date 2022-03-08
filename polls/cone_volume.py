988833fbb258b3fbd4802fc43a95e8d1394b4c74
Prince Boamah <regioths@gmail.com>
2022-03-08 10:07:12 +0000

    A cone volume program

---
+++ polls/cone_volume.py
@@ -0,0 +1,13 @@
+#  A program that finds the volume of a cone using the radius and height
+
+# Author: Prince Oppong Boamah<regioths@gmail.com>
+# Date 8th March, 2022.
+
+PI = 3.141592653589793
+
+radius = int(input())
+height = int(input())
+
+volume = (PI * radius ** 2 * height) / 3
+
+print(volume)
